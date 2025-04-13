b8bff7d9b390cb12868c0fd39149995d9fb385ec
Iftikhar31 <ikala005@ucr.edu>
2025-04-13 10:03:16 -0700

    Update defs.h moved procCount under proc.c section

--- kernel/defs.h
+++ kernel/defs.h
@@ -107,6 +107,8 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+void            print_hello(int);
+int             procCount(void); //CS202 Lab1 counts number of processes
 
 // swtch.S
 void            swtch(struct context*, struct context*);
@@ -190,5 +192,4 @@ void            virtio_disk_intr(void);
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
 
-void            print_hello(int);
-int             procCount(void); //counts number of processes
+
